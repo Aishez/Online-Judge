ce4a2549c8197e0304aa30102df34a608b4a361e
Aishez <aishezsingh@gmail.com>
2023-09-08 16:47:37 +0530

    hi

--- Backend/server.js
+++ Backend/server.js
@@ -1,12 +1,34 @@
-import express from 'express';
+const express = require('express');
+const cors = require('cors');
+const mongoose = require('mongoose');
 
 const app = express();
+const port = 5000;
 
 app.get('/', (req, res) => {
-
-    res.send('Welcome to mern');
+  res.send('Welcome to mern');
 });
 
-app.listen(8000 , () =>{
-    console.log("server is running on port 8000!");
-})
+app.listen(8000, () => {
+  console.log("Server is running on port 8000!");
+});
+
+// Middleware
+app.use(cors());
+app.use(express.json());
+
+// MongoDB connection
+const mongoURI = 'your-mongodb-uri';
+mongoose.connect(mongoURI, { useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology: true });
+const connection = mongoose.connection;
+connection.once('open', () => {
+  console.log('MongoDB database connection established successfully');
+});
+
+// Routes
+const authRouter = require('./routes/auth');
+app.use('/api/auth', authRouter);
+
+app.listen(port, () => {
+  console.log(`Server is running on port: ${port}`);
+});
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,20 @@
-import logo from './logo.svg';
+import React from 'react';
+import { Router, Route } from 'react-router-dom';
+import { createBrowserHistory } from 'history'; // Import createBrowserHistory
+
+import Login from './components/Login';
+import Signup from './components/Signup';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Hello my first react app
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <>
+            <Router history={createBrowserHistory()}>
+                <Route path="/login" element={<Login />} />
+                <Route path="/signup" element={<Signup />} />
+            </Router>
+        </>
+    );
 }
 
 export default App;
